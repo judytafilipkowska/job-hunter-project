66d6f712108eddd167461379f7f26f2ba58d35c2
Judyta Filipkowska <judytafilipkowska@gmail.com>
2021-11-23 16:11:26 +0000

    remove extra code2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 // const PORT = process.env.PORT || 3000;
-const PORT = 9999
+const PORT = process.env.PORT || 9999
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
