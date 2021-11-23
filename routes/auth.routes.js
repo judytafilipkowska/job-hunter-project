662e2e9e32568baecb5d91ab9a6f859d38a1b5f3
Margarida Krupsaya <gomesfmargarida@gmail.com>
2021-11-23 11:48:58 +0000

    job routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,53 +6,77 @@ const saltRounds = 10;
 
 //ROUTES
 
-// GET  /signup form 
+// GET  /signup form
 router.get("/signup-jobseeker", (req, res) => {
-    res.render("auth/signup-form-js");
-  });
+  res.render("auth/signup-form-js");
+});
 
 router.get("/signup-employer", (req, res) => {
-   res.render("auth/signup-form");
-  });
+  res.render("auth/signup-form");
+});
 //POST /signup
 
-router.post("/signup-jobseeker", (req,res) => {
-    const {accountType, username, password, email, firstName, lastName, location, addPicture, addResume} = req.body;
+router.post("/signup-jobseeker", (req, res) => {
+  const {
+    accountType,
+    username,
+    password,
+    email,
+    firstName,
+    lastName,
+    location,
+    addPicture,
+    addResume,
+  } = req.body;
 
-    const accountTypeNotProvided = !accountType || accountType === "" ;
-    const usernameNotProvided = !username || username === "";
-    const passwordNotProvided = !password || password === "";
-    const emailNotProvided = !email || email === "";
-    const firstNameNotProvided = !firstName || firstName === "";
-    const lastNameNotProvided = !lastName || lastName === "";
-    const locationNotProvided = !location || location === "";
+  const accountTypeNotProvided = !accountType || accountType === "";
+  const usernameNotProvided = !username || username === "";
+  const passwordNotProvided = !password || password === "";
+  const emailNotProvided = !email || email === "";
+  const firstNameNotProvided = !firstName || firstName === "";
+  const lastNameNotProvided = !lastName || lastName === "";
+  const locationNotProvided = !location || location === "";
 
-    if (accountTypeNotProvided || usernameNotProvided || passwordNotProvided || emailNotProvided || firstNameNotProvided || lastNameNotProvided || locationNotProvided) {
-        res.render("auth/signup-form-js", {
-            errorMessage: "Please provide required information"
-        });
-        return
-    }
- 
-    User.findOne({ username: username })
+  if (
+    accountTypeNotProvided ||
+    usernameNotProvided ||
+    passwordNotProvided ||
+    emailNotProvided ||
+    firstNameNotProvided ||
+    lastNameNotProvided ||
+    locationNotProvided
+  ) {
+    res.render("auth/signup-form-js", {
+      errorMessage: "Please provide required information",
+    });
+    return;
+  }
+
+  User.findOne({ username: username })
     .then((foundUser) => {
       if (foundUser) {
         throw new Error("The username is taken");
       }
-    
+
       return bcrypt.genSalt(saltRounds);
     })
     .then((salt) => {
- 
       return bcrypt.hash(password, salt);
     })
     .then((hashedPassword) => {
-     
-    return User.create({ accountType: accountType, username: username, password: hashedPassword, email: email, firstName: firstName, lastName: lastName, location: location, addPicture: addPicture, addResume: addResume});
-
+      return User.create({
+        accountType: accountType,
+        username: username,
+        password: hashedPassword,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        location: location,
+        addPicture: addPicture,
+        addResume: addResume,
+      });
     })
     .then((createdUser) => {
-    
       res.redirect("/");
     })
     .catch((err) => {
@@ -62,10 +86,21 @@ router.post("/signup-jobseeker", (req,res) => {
     });
 });
 
-router.post("/signup-employer", (req,res) => {
-  const {accountType, username, password, email, firstName, lastName, companyName, location, addPicture, addResume} = req.body;
+router.post("/signup-employer", (req, res) => {
+  const {
+    accountType,
+    username,
+    password,
+    email,
+    firstName,
+    lastName,
+    companyName,
+    location,
+    addPicture,
+    addResume,
+  } = req.body;
 
-  const accountTypeNotProvided = !accountType || accountType === "" ;
+  const accountTypeNotProvided = !accountType || accountType === "";
   const usernameNotProvided = !username || username === "";
   const passwordNotProvided = !password || password === "";
   const emailNotProvided = !email || email === "";
@@ -74,79 +109,93 @@ router.post("/signup-employer", (req,res) => {
   const lastNameNotProvided = !lastName || lastName === "";
   const locationNotProvided = !location || location === "";
 
-  if (accountTypeNotProvided || usernameNotProvided || passwordNotProvided || companyNameNotProvided || emailNotProvided || firstNameNotProvided || lastNameNotProvided || locationNotProvided) {
-      res.render("auth/signup-form", {
-          errorMessage: "Please provide required information"
-      });
-      return
+  if (
+    accountTypeNotProvided ||
+    usernameNotProvided ||
+    passwordNotProvided ||
+    companyNameNotProvided ||
+    emailNotProvided ||
+    firstNameNotProvided ||
+    lastNameNotProvided ||
+    locationNotProvided
+  ) {
+    res.render("auth/signup-form", {
+      errorMessage: "Please provide required information",
+    });
+    return;
   }
 
   User.findOne({ username: username })
-  .then((foundUser) => {
-    if (foundUser) {
-      throw new Error("The username is taken");
-    }
-  
-    return bcrypt.genSalt(saltRounds);
-  })
-  .then((salt) => {
+    .then((foundUser) => {
+      if (foundUser) {
+        throw new Error("The username is taken");
+      }
 
-    return bcrypt.hash(password, salt);
-  })
-  .then((hashedPassword) => {
-   
-  return User.create({ accountType: accountType, username: username, password: hashedPassword, email: email, firstName: firstName, lastName: lastName, companyName: companyName, location: location, addPicture: addPicture});
-
-  })
-  .then((createdUser) => {
-  
-    res.redirect("/");
-  })
-  .catch((err) => {
-    res.render("auth/signup-form", {
-      errorMessage: err.message || "Error while trying to sign up",
+      return bcrypt.genSalt(saltRounds);
+    })
+    .then((salt) => {
+      return bcrypt.hash(password, salt);
+    })
+    .then((hashedPassword) => {
+      return User.create({
+        accountType: accountType,
+        username: username,
+        password: hashedPassword,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        companyName: companyName,
+        location: location,
+        addPicture: addPicture,
+      });
+    })
+    .then((createdUser) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.render("auth/signup-form", {
+        errorMessage: err.message || "Error while trying to sign up",
+      });
     });
-  });
 });
 
-
 router.get("/login", (req, res) => {
-  res.render("auth/login-form")
-})
+  res.render("auth/login-form");
+});
 
 router.post("/login", (req, res) => {
-  const {username, password} = req.body;
+  const { username, password } = req.body;
 
   const usernameNotProvided = !username || username === "";
   const passwordNotProvided = !password || password === "";
-  
-  if ( usernameNotProvided || passwordNotProvided ) {
+
+  if (usernameNotProvided || passwordNotProvided) {
     res.render("auth/login-form", {
-     errorMessage: "Please provide username and/or password" 
-  });
-  return
-}
-let user;
-User.findOne({ username: username})
-.then ((foundUser) => {
-  if (!foundUser) {
-    throw new Error("Wrong credentials");
+      errorMessage: "Please provide username and/or password",
+    });
+    return;
   }
-  return bcrypt.compare(password, foundUser.password);
-})
-.then((passwordCorrect) => {
-  if(!correctPassword) {
-    throw new Error("Wrong credentials");
-  } else if (passwordCorrect) {
-    req.session.user = user;
-    res.redirect("/")
-  }
-})
-.catch((err) => {
-  res.render("auth/login-form", {
-    errorMessage: err.message || "Provide username and password."
-  });
-});
+  let user;
+  User.findOne({ username: username })
+    .then((foundUser) => {
+      if (!foundUser) {
+        throw new Error("Wrong credentials");
+      }
+      return bcrypt.compare(password, foundUser.password);
+    })
+    .then((passwordCorrect) => {
+      if (!correctPassword) {
+        throw new Error("Wrong credentials");
+      } else if (passwordCorrect) {
+        req.session.user = user;
+        res.redirect("/");
+      }
+    })
+    .catch((err) => {
+      res.render("auth/login-form", {
+        errorMessage: err.message || "Provide username and password.",
+      });
+    });
 });
 
 module.exports = router;
--- routes/job.routes.js
+++ routes/job.routes.js
@@ -1,6 +1,56 @@
 const router = require("express").Router();
 const Job = require("../models/job.model");
 
-//Routes 
+//GET JOB PANEL -> needs get cause its first display of the page
+router.get("/jobs/job-panel", (req, res) => {
+  const { addedBy, position, remote, location, wage, description } = req.body;
 
-//
+  Job.find().then((foundedAll) => {
+    res.render("jobs/job-panel", foundedAll).catch((err) => console.log(err));
+  });
+});
+
+//GET ADD JOB
+router.get("/jobs/add-job", (req, res) => {
+  res.render("jobs/add-job");
+});
+
+//POST ADD JOB
+router.post("/jobs/add-job", (req, res) => {
+  const { addedBy, position, remote, location, wage, description } = req.body;
+
+  Job.create({ addedBy, position, remote, location, wage, description })
+    .then((createdJob) => {
+      // Redirect to the page with the job details
+      res.redirect(`/jobs/job-panel/${createdJob._id}`);
+    })
+    .catch((err) => console.log(err));
+});
+
+//GET EDIT JOB
+router.get("/jobs/:jobId/edit-job", (req, res) => {
+  const jobId = req.params.jobId;
+
+  Job.findById(jobId)
+    .then((jobs) => {
+      res.render("jobs/edit-job", { editedJobs: jobs });
+    })
+    .catch((err) => console.log(err));
+});
+
+//POST EDIT JOB
+router.post("/jobs/:jobId/edit-job", (req, res) => {
+  const jobId = req.params.jobId;
+  const { addedBy, position, remote, location, wage, description } = req.body;
+  Job.findByIdAndUpdate(
+    { addedBy, position, remote, location, wage, description },
+    { new: true }
+  )
+
+    .then((updatedJob) => {
+      res.redirect(`/jobs/details-job/${jobId}`);
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
